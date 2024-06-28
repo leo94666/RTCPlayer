a78de64457ad5f60ba7a2b007008adfc88f80df7
li <719947897@qq.com>
2024-06-28 09:44:31 +0800

    开发配置baseUrl接口

--- RTCPlayer/src/main/java/com/rtc/core/RTCCore.kt
+++ RTCPlayer/src/main/java/com/rtc/core/RTCCore.kt
@@ -10,13 +10,13 @@ object RTCCore {
     fun init(baseUrl: String) {
         BASE_URL = baseUrl
         PLATFORM = Platform.ZLMediaKit
-        NativeLib.instance.init(baseUrl, Platform.ZLMediaKit)
+        NativeLib.instance.init(baseUrl)
     }
 
-    fun init(baseUrl: String, platform: Platform = Platform.ZLMediaKit) {
-        BASE_URL = baseUrl
-        PLATFORM = platform
-        NativeLib.instance.init(baseUrl, platform)
-    }
+//    fun init(baseUrl: String, platform: Platform = Platform.ZLMediaKit) {
+//        BASE_URL = baseUrl
+//        PLATFORM = platform
+//        NativeLib.instance.init(baseUrl, platform)
+//    }
 
 }
